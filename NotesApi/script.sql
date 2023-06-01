0fe5af101123ec8995f12ac0e36aff5575651837
golybock <teplovartem3094@gmail.com>
2023-06-01 22:10:18 +0300

    updated

--- NotesApi/script.sql
+++ NotesApi/script.sql
@@ -6,40 +6,15 @@ create table if not exists tag
         unique
 );
 
-create table if not exists note_user
+create table if not exists users
 (
-    id            integer default nextval('user_id_seq'::regclass) not null
-        constraint user_pkey
-            primary key,
-    email         varchar(500)                                     not null
+    id            serial       not null
+        primary key,
+    email         varchar(500) not null
         constraint user_email_key
             unique,
     password_hash text,
     name          varchar(500)
-);
-
-create table if not exists note
-(
-    id            serial
-        primary key,
-    header        varchar(250)             not null,
-    creation_date timestamp with time zone not null,
-    edited_date   timestamp with time zone not null,
-    source_path   text,
-    user_id       integer
-        references users,
-    guid          uuid                     not null
-        unique
-);
-
-create table if not exists note_tag
-(
-    id      serial
-        primary key,
-    note_id integer
-        references note,
-    tag_id  integer
-        references tag
 );
 
 create table if not exists tokens
@@ -53,5 +28,55 @@ create table if not exists tokens
     creation_date timestamp with time zone default now() not null,
     ip            inet,
     active        boolean                  default true
+);
+
+create table if not exists permissions_level
+(
+    id   serial
+        primary key,
+    name varchar(150)
+);
+
+create table if not exists note_type
+(
+    id   serial
+        primary key,
+    name varchar(150)
+);
+
+create table if not exists note
+(
+    id            uuid                     not null
+        primary key,
+    header        varchar(250)             not null,
+    creation_date timestamp with time zone not null,
+    edited_date   timestamp with time zone not null,
+    source_path   text,
+    type_id       integer
+        references note_type,
+    owner_id      integer
+        references users
+);
+
+create table if not exists note_tag
+(
+    id      serial
+        primary key,
+    tag_id  integer
+        references tag,
+    note_id uuid
+        references note (id)
+);
+
+create table if not exists shared_notes
+(
+    id                   serial
+        primary key,
+    note_id              uuid
+        references note (id),
+    user_id              integer
+        references users,
+    permissions_level_id integer
+        references permissions_level
 );
 
