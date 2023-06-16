a490a09cedc984bfa4397e9a4fa86a3293ffd430
inerer <arshinov04@gmail.com>
2023-06-16 10:45:52 +0300

    redesign buttons

--- NotesApi/ClientApp/src/components/note/CreateNoteDialog.js
+++ NotesApi/ClientApp/src/components/note/CreateNoteDialog.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import NoteApi from "../../api/note/NoteApi";
 import {NoteBlank} from "../../models/blank/note/NoteBlank";
 
@@ -15,74 +14,70 @@ export default class CreateNoteDialog extends React.Component {
     }
 
     async create() {
-        
+
         let noteBlank = new NoteBlank(this.state.name, "");
-        
+
         let id = await NoteApi.createNote(noteBlank)
 
-        this.setState({name : ""})
-        
+        this.setState({name: ""})
+
         this.props.open(id)
 
         this.props.handleClose()
     }
 
     render() {
-        return (
-            <Modal
-                show={this.props.show}
-                onHide={this.props.handleClose}
-                backdrop="static"
-                centered>
+        return (<Modal
+            show={this.props.show}
+            onHide={this.props.handleClose}
+            backdrop="static"
+            centered>
 
-                <Modal.Header closeButton>
-                    <Modal.Title>Create note</Modal.Title>
-                </Modal.Header>
+            <Modal.Header closeButton>
+                <Modal.Title>Create note</Modal.Title>
+            </Modal.Header>
 
-                <Modal.Body>
-                    
-                    <Form>
-                        
-                        <Form.Group className="mb-3">
-                            <Form.Label>Note name</Form.Label>
-                            
-                            <Form.Control
-                                type="text"
-                                placeholder="name"
-                                autoFocus
-                                required
-                                value={this.state.name}
-                                onChange={(e) => {
-                                    this.setState({
-                                        name: e.target.value
-                                    })
-                                }}
-                            />
-                            
-                        </Form.Group>
-                        
-                    </Form>
-                    
-                </Modal.Body>
+            <Modal.Body>
 
-                <Modal.Footer>
+                <Form>
 
-                    <Button variant="secondary" 
-                            className="btn"
-                             onClick={this.props.handleClose}>
-                        Close
-                    </Button>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Note name</Form.Label>
 
-                    <Button variant="primary"
-                            className="btn btn-primary-note"
-                            onClick={async () => await this.create()}>
-                        Save Changes
-                    </Button>
+                        <Form.Control
+                            type="text"
+                            placeholder="name"
+                            autoFocus
+                            required
+                            value={this.state.name}
+                            onChange={(e) => {
+                                this.setState({
+                                    name: e.target.value
+                                })
+                            }}
+                        />
 
-                </Modal.Footer>
+                    </Form.Group>
 
-            </Modal>
-        )
+                </Form>
+
+            </Modal.Body>
+
+            <Modal.Footer>
+
+                <button className="btn btn-primary-note"
+                        onClick={this.props.handleClose}>
+                    Close
+                </button>
+
+                <button className="btn btn-primary-note"
+                        onClick={async () => await this.create()}>
+                    Save Changes
+                </button>
+
+            </Modal.Footer>
+
+        </Modal>)
     }
 
 }
--- NotesApi/ClientApp/src/components/note/NoteCard.js
+++ NotesApi/ClientApp/src/components/note/NoteCard.js
@@ -3,37 +3,30 @@ import Card from "react-bootstrap/Card";
 import Cat from "../../resources/cat.webp";
 import Button from "react-bootstrap/Button";
 
-export default class NoteCard extends React.Component{
-    
+export default class NoteCard extends React.Component {
+
     render() {
-        return(
-            <Card style={{width: '16rem'}} key={this.props.note.id}>
-                
+        return (
+            <Card style={{width: '16rem'}}
+                  key={this.props.note.id}
+                  onClick={() => this.props.open(this.props.note.id)}>
+
                 <Card.Img variant="top"
                           style={{padding: "15px"}}
                           src={Cat}/>
-                
+
                 <Card.Title style={{color: "black", margin: 5}}>
                     {this.props.note.header}
                 </Card.Title>
-                
+
                 <Card.Body>
-                    
+
                     <Card.Text style={{color: "black"}}>
                         {this.props.note.text}
                     </Card.Text>
-                    
+
                 </Card.Body>
-                
-                <Card.Footer>
-                    
-                    <button className="btn transparent-btn"
-                            onClick={() => this.props.open(this.props.note.id)}>
-                        Open
-                    </button>
-                    
-                </Card.Footer>
-                
+
             </Card>
         )
     }
