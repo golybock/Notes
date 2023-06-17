5c45a6f9243ec5bd8668f821c2d880ba3b50181b
golybock <teplovartem3094@gmail.com>
2023-06-17 22:29:12 +0300

    mini cleanup

--- NotesApi/Program.cs
+++ NotesApi/Program.cs
@@ -1,48 +1,58 @@
 using Microsoft.AspNetCore.Authorization;
 using NotesApi.RefreshCookieAuthScheme;
 
+void SetRefreshCookieAuth(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddAuthentication(RefreshCookieDefaults.AuthenticationScheme)
+        .AddRefreshCookie(
+            RefreshCookieDefaults.AuthenticationScheme,  RefreshCookieDefaults.AuthenticationScheme, options =>
+            {
+                options.ConnectionString = configuration.GetConnectionString("notes")!;
+                options.Secret = configuration["JWT:Secret"];
+                options.TokenLifeTimeInMinutes = int.Parse(configuration["JWT:TokenValidityInMinutes"]);
+                options.RefreshTokenLifeTimeInDays = Int32.Parse(configuration["JWT:RefreshTokenValidityInDays"]);
+                options.ValidIssuer = configuration["JWT:ValidIssuer"];
+                options.ValidAudience = configuration["JWT:ValidAudience"];
+                options.ValidateLifetime = true;
+            });
+
+    services.AddAuthorization(options =>
+    {
+        options.DefaultPolicy = new AuthorizationPolicyBuilder()
+            .AddAuthenticationSchemes(RefreshCookieDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser()
+            .Build();
+    });
+}
+
+void SetCors(IServiceCollection services)
+{
+    // Default Policy
+    services.AddCors(options =>
+    {
+        options.AddDefaultPolicy(
+            corsPolicyBuilder =>
+            {
+                corsPolicyBuilder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            });
+    }); 
+}
+
 var builder = WebApplication.CreateBuilder(args);
+
+SetRefreshCookieAuth(builder.Services, builder.Configuration);
+
+SetCors(builder.Services);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(RefreshCookieDefaults.AuthenticationScheme)
-    .AddRefreshCookie(
-        RefreshCookieDefaults.AuthenticationScheme,  RefreshCookieDefaults.AuthenticationScheme, options =>
-        {
-            options.ConnectionString = builder.Configuration.GetConnectionString("notes")!;
-            options.Secret = builder.Configuration["JWT:Secret"];
-            options.TokenLifeTimeInMinutes = int.Parse(builder.Configuration["JWT:TokenValidityInMinutes"]);
-            options.RefreshTokenLifeTimeInDays = Int32.Parse(builder.Configuration["JWT:RefreshTokenValidityInDays"]);
-            options.ValidIssuer = builder.Configuration["JWT:ValidIssuer"];
-            options.ValidAudience = builder.Configuration["JWT:ValidAudience"];
-            options.ValidateLifetime = true;
-        });
-
-builder.Services.AddAuthorization(options =>
-{
-    options.DefaultPolicy = new AuthorizationPolicyBuilder()
-        .AddAuthenticationSchemes(RefreshCookieDefaults.AuthenticationScheme)
-        .RequireAuthenticatedUser()
-        .Build();
-});
-
-// Default Policy
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        corsPolicyBuilder =>
-        {
-            corsPolicyBuilder
-                .WithOrigins("http://localhost:3000")
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
-        });
-});
 
 var app = builder.Build();
 
